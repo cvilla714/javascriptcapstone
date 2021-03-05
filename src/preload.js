8914071d9d6bbf671c5a65706985c1e8b914b54a
Cosmel Villalobos <cosmelvg@gmail.com>
2021-03-05 09:55:08 -0500

    created the preload content loading the platforms, coins, player and moutains

---
+++ src/preload.js
@@ -0,0 +1,77 @@
+import player from "./images/player.png";
+import platform from "./images/platform.png";
+import coin from "./images/coin.png";
+import fire from "./images/fire.png";
+import mountain from "./images/mountain.png";
+
+class preloadGame extends Phaser.Scene {
+  constructor() {
+    super("PreloadGame");
+  }
+  preload() {
+    this.load.image("platform", platform);
+
+    // player is a sprite sheet made by 24x48 pixels
+    this.load.spritesheet("player", player, {
+      frameWidth: 24,
+      frameHeight: 48,
+    });
+
+    // the coin is a sprite sheet made by 20x20 pixels
+    this.load.spritesheet("coin", coin, {
+      frameWidth: 20,
+      frameHeight: 20,
+    });
+
+    // the firecamp is a sprite sheet made by 32x58 pixels
+    this.load.spritesheet("fire", fire, {
+      frameWidth: 40,
+      frameHeight: 70,
+    });
+
+    // mountains are a sprite sheet made by 512x512 pixels
+    this.load.spritesheet("mountain", mountain, {
+      frameWidth: 512,
+      frameHeight: 512,
+    });
+  }
+  create() {
+    // setting player animation
+    this.anims.create({
+      key: "run",
+      frames: this.anims.generateFrameNumbers("player", {
+        start: 0,
+        end: 1,
+      }),
+      frameRate: 8,
+      repeat: -1,
+    });
+
+    // setting coin animation
+    this.anims.create({
+      key: "rotate",
+      frames: this.anims.generateFrameNumbers("coin", {
+        start: 0,
+        end: 5,
+      }),
+      frameRate: 15,
+      yoyo: true,
+      repeat: -1,
+    });
+
+    // setting fire animation
+    this.anims.create({
+      key: "burn",
+      frames: this.anims.generateFrameNumbers("fire", {
+        start: 0,
+        end: 4,
+      }),
+      frameRate: 15,
+      repeat: -1,
+    });
+
+    this.scene.start("PlayGame");
+  }
+}
+
+export default preloadGame;
