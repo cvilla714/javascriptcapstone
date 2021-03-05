3ca12f89610858e206beab2071d5fd1c468e1896
Cosmel Villalobos <cosmelvg@gmail.com>
2021-03-05 09:55:55 -0500

    created the file for resizing the game

---
+++ src/resize.js
@@ -0,0 +1,18 @@
+import { game, gameOptions } from "./game";
+
+function resize() {
+  let canvas = document.querySelector("canvas");
+  let windowWidth = window.innerWidth;
+  let windowHeight = window.innerHeight;
+  let windowRatio = windowWidth / windowHeight;
+  let gameRatio = game.config.width / game.config.height;
+  if (windowRatio < gameRatio) {
+    canvas.style.width = windowWidth + "px";
+    canvas.style.height = windowWidth / gameRatio + "px";
+  } else {
+    canvas.style.width = windowHeight * gameRatio + "px";
+    canvas.style.height = windowHeight + "px";
+  }
+}
+
+export default resize;
