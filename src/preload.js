06b735882885895bba1bc2810b4d963528545800
Cosmel Villalobos <cosmelvg@gmail.com>
2021-03-05 16:48:38 -0500

    added the music for the game

--- src/preload.js
+++ src/preload.js
@@ -5,12 +5,14 @@ import platform from "./images/platform.png";
 import coin from "./images/coin.png";
 import fire from "./images/fire.png";
 import mountain from "./images/mountain.png";
+import music from "./DanceandJump.ogg";
 
 class preloadGame extends Phaser.Scene {
   constructor() {
     super("PreloadGame");
   }
   preload() {
+    this.load.audio("music", music);
     this.load.image("platform", platform);
 
     // player is a sprite sheet made by 24x48 pixels
@@ -38,6 +40,19 @@ class preloadGame extends Phaser.Scene {
     });
   }
   create() {
+    //music
+    this.music = this.sound.add("music");
+
+    var musicConfig = {
+      mute: false,
+      volume: 1,
+      rate: 1,
+      detune: 0,
+      seek: 0,
+      loop: false,
+      delay: 0,
+    };
+    this.music.play(musicConfig);
     // setting player animation
     this.anims.create({
       key: "run",
