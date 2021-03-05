a0a8bf7bf6ac379f4985ac8aa2ec599b9d87393f
Cosmel Villalobos <cosmelvg@gmail.com>
2021-03-04 20:56:48 -0500

    changed to name instead of hashtag when creating the images folder

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
 
   output: {
     path: path.resolve(__dirname, "dist"),
-    assetModuleFilename: "images/[hash][ext][query]",
+    assetModuleFilename: "images/[name][ext][query]",
   },
 
   module: {
