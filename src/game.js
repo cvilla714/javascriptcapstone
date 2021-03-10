a5f9d5b9b316df6df5321ebe9878e6edaa17372d
Cosmel Villalobos <cosmelvg@gmail.com>
2021-03-10 16:35:05 -0500

    added the pauseScene to the scene array

--- src/game.js
+++ src/game.js
@@ -4,6 +4,7 @@ import playGame from "./playgame";
 import resize from "./resize";
 import MenuScene from "./MenuScene";
 import ScoreScene from "./ScoreScene";
+import PauseScene from "./PauseScene";
 
 let game;
 
@@ -56,7 +57,7 @@ window.onload = function () {
     type: Phaser.AUTO,
     width: 1334,
     height: 750,
-    scene: [preloadGame, MenuScene, ScoreScene, playGame],
+    scene: [preloadGame, MenuScene, ScoreScene, playGame, PauseScene],
     backgroundColor: 0x0c88c7,
 
     // physics settings
