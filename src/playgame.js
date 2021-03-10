8b9a6d41c2f3e9cae822ff4db9120172882a9e65
Cosmel Villalobos <cosmelvg@gmail.com>
2021-03-10 16:35:57 -0500

    added the option to launch the PauseScene when the buttons pause is pressed

--- src/playgame.js
+++ src/playgame.js
@@ -353,6 +353,7 @@ class playGame extends BaseScene {
     pauseButton.on("pointerdown", () => {
       this.physics.pause();
       this.scene.pause();
+      this.scene.launch("PauseScene");
     });
   }
 
