f84b72059cf6849a85c7b7ed2b0d3260f0cfee2e
Cosmel Villalobos <cosmelvg@gmail.com>
2021-03-10 12:48:29 -0500

    changed the colors of the text and added the seupMenuEvents function

--- src/BaseScene.js
+++ src/BaseScene.js
@@ -8,7 +8,7 @@ class BaseScene extends Phaser.Scene {
     this.screenCenter = [game.config.width / 2, game.config.height / 2];
     this.fontSize = 55;
     this.lineHeight = 65;
-    this.fontOptions = { fontSize: `${this.fontSize}px`, fill: `#CD00ff` };
+    this.fontOptions = { fontSize: `${this.fontSize}px`, fill: `#fff` };
   }
 
   create() {
@@ -16,12 +16,13 @@ class BaseScene extends Phaser.Scene {
     // this.scene.start("PlayGame");
   }
 
-  createMenu(menu) {
+  createMenu(menu, setupMenuEvents) {
     let lastMenuPositionY = 0;
     menu.forEach((menuItem) => {
       const menuPosition = [this.screenCenter[0], this.screenCenter[1] + lastMenuPositionY];
-      this.add.text(...menuPosition, menuItem.text, this.fontOptions).setOrigin(0.5, 1);
+      menuItem.textGO = this.add.text(...menuPosition, menuItem.text, this.fontOptions).setOrigin(0.5, 1);
       lastMenuPositionY += this.lineHeight;
+      setupMenuEvents(menuItem);
     });
   }
 }
