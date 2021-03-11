057f4c20fd8f077d2129eae382cb72abf24365e1
Cosmel Villalobos <cosmelvg@gmail.com>
2021-03-11 16:15:38 -0500

    added the function to display the best scores from the api

--- src/ScoreScene.js
+++ src/ScoreScene.js
@@ -1,5 +1,6 @@
 import BaseScene from "./BaseScene";
 import back from "./images/back.png";
+import scoreboard from "./ScoreBoard";
 
 class ScoreScene extends BaseScene {
   constructor(config) {
@@ -7,15 +8,34 @@ class ScoreScene extends BaseScene {
     // super("ScoreScene", confifg);
   }
 
-  prealod() {
+  preload() {
     this.load.image("back", back);
   }
 
   create() {
     super.create();
     this.createBack();
-    const bestScore = localStorage.getItem("bestScore");
-    this.add.text(...this.screenCenter, `Best Score: ${bestScore || 0}`, this.fontOptions).setOrigin(0.5);
+    // const bestScore = localStorage.getItem("bestScore");
+    const bestScore = scoreboard();
+    console.log(bestScore);
+    // bestScore.then((data) => console.log(data));
+    // const bestScore = ScoreList();
+    // this.add.text(...this.screenCenter, `Best Score: ${bestScore || 0}`, this.fontOptions).setOrigin(0.5);
+    let lastMenuPositionY = 0;
+
+    // menu.forEach((menuItem) => {
+    //   const menuPosition = [this.screenCenter[0], this.screenCenter[1] + lastMenuPositionY];
+    //   menuItem.textGO = this.add.text(...menuPosition, menuItem.text, this.fontOptions).setOrigin(0.5, 1);
+    //   lastMenuPositionY += this.lineHeight;
+    //   setupMenuEvents(menuItem);
+
+    bestScore.then((data) => {
+      data.map(({ score, user }) => {
+        const menuPosition = [this.screenCenter[0], this.screenCenter[1] + lastMenuPositionY];
+        this.add.text(...menuPosition, `Best Score: ${score || 0} ${user}`, this.fontOptions).setOrigin(0.5);
+        lastMenuPositionY += this.lineHeight;
+      });
+    });
   }
 
   createBack() {
