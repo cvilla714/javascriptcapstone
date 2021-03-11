b4bb55cd582f7cec44ba09527534a4a5ae1a4847
Cosmel Villalobos <cosmelvg@gmail.com>
2021-03-11 17:58:02 -0500

    refactored the function to get the api information

--- src/ScoreBoard.js
+++ src/ScoreBoard.js
@@ -1,27 +1,14 @@
 const scoreboard = async () => {
   const information = await fetch("https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/:id/scores/");
   const info = await information.json();
-  // console.log(info);
-  // console.log(info.result);
   const names = info.result;
 
-  // console.log(names);
   return names;
-  // console.log(info.result[1]);
-  // let bucket = "";
-  // for (let i = 0; i < names.length; i++) {
-  //   bucket = names[i];
-  //   // console.log(bucket);
-  //   // console.log(names[i]);
-  //   // console.log(bucket.user, bucket.score);
-  // }
-  // return bucket;
 };
 
 scoreboard();
 
 export default scoreboard;
-// export default scoreboard;
 
 // const submit = async (name, score) => {
 //   try {
