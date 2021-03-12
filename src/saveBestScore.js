89cc84ce865373268ada74af1cfa819a959951c5
Cosmel Villalobos <cosmelvg@gmail.com>
2021-03-12 13:11:46 -0500

    created the functions to keep the score

---
+++ src/saveBestScore.js
@@ -0,0 +1,8 @@
+export default function savingyou(score) {
+  const bestScoreText = localStorage.getItem("bestScore");
+  const bestScore = bestScoreText && parseInt(bestScoreText, 10);
+
+  if (!bestScore || score > bestScore) {
+    localStorage.setItem("bestScore", score);
+  }
+}
