51e95964bd5382c30086c5893fdf9130ad06016c
Cosmel Villalobos <cosmelvg@gmail.com>
2021-03-12 14:01:23 -0500

    added the test for ScoreScene

---
+++ src/test/ScoreScene.test.js
@@ -0,0 +1,10 @@
+import "jest-canvas-mock";
+import BaseScene from "../BaseScene";
+
+test("ScoreScene is a subclass of container", () => {
+  expect(BaseScene.prototype instanceof BaseScene).toBe(false);
+});
+
+test("ScoreScene is not a subclass of container", () => {
+  expect(BaseScene.prototype instanceof Phaser.GameObjects.Container).not.toBe(true);
+});
