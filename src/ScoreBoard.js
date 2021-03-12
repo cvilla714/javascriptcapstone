ddd443267cd04d0f9059586849b02893f0243f35
Cosmel Villalobos <cosmelvg@gmail.com>
2021-03-12 15:56:56 -0500

    added try and catch the error from the API incase no requests were made

--- src/ScoreBoard.js
+++ src/ScoreBoard.js
@@ -1,66 +1,15 @@
 const scoreboard = async () => {
-  const information = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/:id/scores/');
-  const info = await information.json();
-  const names = info.result;
+  try {
+    const information = await fetch("https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/:id/scores/");
+    const info = await information.json();
+    const names = info.result;
 
-  return names;
+    return names;
+  } catch (e) {
+    throw Error("No info to display");
+  }
 };
 
 scoreboard();
 
 export default scoreboard;
-
-// const submit = async (name, score) => {
-//   try {
-//     const result = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/:id/scores/`, {
-//       method: "POST",
-//       headers: {
-//         Accept: "application/json",
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify({
-//         user: name,
-//         score: Number(score),
-//       }),
-//     });
-
-//     return result.json();
-//   } catch (error) {
-//     return error.json();
-//   }
-// };
-
-// const ScoreList = async () => {
-//   try {
-//     const scores = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/:id/scores/`, {
-//       method: "GET",
-//       headers: {
-//         Accept: "application/json",
-//         "Content-Type": "application/json",
-//       },
-//     });
-//     return scores.json();
-//   } catch (error) {
-//     return error.json();
-//   }
-// };
-
-// ScoreList();
-
-// async function getKey() {
-//   try {
-//     const response = await fetch("https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/", {
-//       method: "POST",
-//       headers: {
-//         Accept: "application/json",
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify({
-//         name: "Endless Game",
-//       }),
-//     });
-//     return response;
-//   } catch (error) {
-//     return error;
-//   }
-// }
