f986312d0d7295c3b137ac0d2ce36cda4e816170
Cosmel Villalobos <cosmelvg@gmail.com>
2021-03-12 13:38:58 -0500

    added test for the BaseScene

--- src/test/BaseScene.test.js
+++ src/test/BaseScene.test.js
@@ -1,12 +1,11 @@
 import "jest-canvas-mock";
 import Phaser from "phaser";
 import BaseScene from "../BaseScene";
-import MenuScene from "../MenuScene";
 
-test("MenuScene a subclass of container", () => {
-  expect(MenuScene.prototype instanceof BaseScene).toBe(true);
+test("BaseScene is a subclass of container", () => {
+  expect(BaseScene.prototype instanceof Phaser.GameObjects.Container).toBe(false);
 });
 
-test("MenuScene a subclass of container", () => {
-  expect(MenuScene.prototype instanceof Phaser.Scene).not.toBe(false);
+test("BaseScene is not a subclass of container", () => {
+  expect(BaseScene.prototype instanceof Phaser.GameObjects.Container).not.toBe(true);
 });
