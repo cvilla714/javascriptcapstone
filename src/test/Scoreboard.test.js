dadda688edc235ad34a07d13ac1230cafc6a6165
Cosmel Villalobos <cosmelvg@gmail.com>
2021-03-12 15:57:09 -0500

    added the test for the api

---
+++ src/test/Scoreboard.test.js
@@ -0,0 +1,21 @@
+import "jest-canvas-mock";
+import regeneratorRuntime from "regenerator-runtime";
+import scoreboard from "../ScoreBoard";
+
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve({ result: {} }),
+  })
+);
+
+beforeEach(() => {
+  fetch.mockClear();
+});
+
+describe("Requestion the information from the API", () => {
+  it("The result should be an Object", async () => {
+    const result = await scoreboard();
+
+    expect(typeof result).toBe("object");
+  });
+});
