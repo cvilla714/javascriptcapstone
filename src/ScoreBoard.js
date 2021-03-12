1ac14296f31be470867fcdfdd743d60736fea088
Cosmel Villalobos <cosmelvg@gmail.com>
2021-03-12 16:01:13 -0500

    fiixed linter errors

--- src/ScoreBoard.js
+++ src/ScoreBoard.js
@@ -1,12 +1,12 @@
 const scoreboard = async () => {
   try {
-    const information = await fetch("https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/:id/scores/");
+    const information = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/:id/scores/');
     const info = await information.json();
     const names = info.result;
 
     return names;
   } catch (e) {
-    throw Error("No info to display");
+    throw Error('No info to display');
   }
 };
 
--- src/test/Scoreboard.test.js
+++ src/test/Scoreboard.test.js
@@ -1,21 +1,20 @@
-import "jest-canvas-mock";
-import regeneratorRuntime from "regenerator-runtime";
-import scoreboard from "../ScoreBoard";
+/* eslint-disable no-unused-vars */
+import 'jest-canvas-mock';
+import regeneratorRuntime from 'regenerator-runtime';
+import scoreboard from '../ScoreBoard';
 
-global.fetch = jest.fn(() =>
-  Promise.resolve({
-    json: () => Promise.resolve({ result: {} }),
-  })
-);
+global.fetch = jest.fn(() => Promise.resolve({
+  json: () => Promise.resolve({ result: {} }),
+}));
 
 beforeEach(() => {
   fetch.mockClear();
 });
 
-describe("Requestion the information from the API", () => {
-  it("The result should be an Object", async () => {
+describe('Requestion the information from the API', () => {
+  it('The result should be an Object', async () => {
     const result = await scoreboard();
 
-    expect(typeof result).toBe("object");
+    expect(typeof result).toBe('object');
   });
 });
