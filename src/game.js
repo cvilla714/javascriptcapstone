4ad5bba2832a322c5c3d9f86baf16ce56760fa36
Cosmel Villalobos <cosmelvg@gmail.com>
2021-03-13 18:05:25 -0500

    added the resize function for the game

--- src/game.js
+++ src/game.js
@@ -1,12 +1,27 @@
 import Phaser from 'phaser';
 import preloadGame from './preload';
 import playGame from './playgame';
-import resize from './resize';
 import MenuScene from './MenuScene';
 import ScoreScene from './ScoreScene';
 import PauseScene from './PauseScene';
 
 let game;
+
+const resize = () => {
+  const canvas = document.querySelector('canvas');
+  const windowWidth = window.innerWidth;
+  const windowHeight = window.innerHeight;
+  const windowRatio = windowWidth / windowHeight;
+  const gameRatio = game.config.width / game.config.height;
+  if (windowRatio < gameRatio) {
+    canvas.style.width = `${windowWidth}px`;
+    canvas.style.height = `${windowWidth / gameRatio}px`;
+  } else {
+    canvas.style.width = `${windowHeight * gameRatio}px`;
+    canvas.style.height = `${windowHeight}px`;
+  }
+};
+
 
 window.onload = function () {
   const gameConfig = {
@@ -24,5 +39,3 @@ window.onload = function () {
   resize();
   window.addEventListener('resize', resize, false);
 };
-
-export { game };
