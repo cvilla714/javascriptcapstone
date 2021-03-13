09f250dc9c74c2ae4ae4f12452da3f53f3f9981e
Cosmel Villalobos <cosmelvg@gmail.com>
2021-03-13 17:50:44 -0500

    splitted the code in differnt lines to avoid max-len rule

--- src/BaseScene.js
+++ src/BaseScene.js
@@ -16,7 +16,8 @@ class BaseScene extends Phaser.Scene {
     let lastMenuPositionY = 0;
     menu.forEach((menuItem) => {
       const menuPosition = [this.screenCenter[0], this.screenCenter[1] + lastMenuPositionY];
-      menuItem.textGO = this.add.text(...menuPosition, menuItem.text, this.fontOptions).setOrigin(0.5, 1);
+      menuItem.textGO = this.add
+        .text(...menuPosition, menuItem.text, this.fontOptions).setOrigin(0.5, 1);
       lastMenuPositionY += this.lineHeight;
       setupMenuEvents(menuItem);
     });
